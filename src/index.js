364c0573bc7f2a218f93ef4b0490a287e91abfb0
James Edmonds <james@deckchairlabs.com>
2018-01-09 17:58:42 +1030

    prettier dist files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import list from './list'
+import get from './get'
 import add from './add'
+import update from './update'
+import remove from './remove'
 
 export default {
-  add
+  list,
+  get,
+  add,
+  update,
+  remove
 }
